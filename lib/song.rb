0529c45529ca076b55f0eb9e1b49835c10a6a56a
Erin Stansbury <ekstansbury@gmail.com>
2020-02-12 17:47:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,6 +16,12 @@ class Song
     song
   end
 
+  def self.new_by_name(title)
+    song = self.new
+    song.name = title
+    song
+  end
+
   def self.create_by_name(title)
     song = self.create
     song.name = title
